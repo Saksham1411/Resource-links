4fb653e3f30496ed1e9758be2ed4d099dfdb62be
Saksham1411 <sakshamsamgarg1411@gmail.com>
2024-11-30 16:53:02 +0530

    cards added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BackgroundBeamsWithCollision } from "./components/ui/background-effect2";
 import { TypewriterEffectSmooth } from "./components/ui/typewriter-effect";
+import CardsData from "./utils/cards-data";
 
 function App() {
   const words = [
@@ -12,12 +13,19 @@ function App() {
       className: "text-neutral-400",
     },
   ];
+  console.log(CardsData);
+
   return (
     <div className="">
       <BackgroundBeamsWithCollision>
         <TypewriterEffectSmooth words={words} />
         <div className=" w-80">
-          
+          {CardsData.map((data, idx) => (
+            <div id={idx} className="card">
+              {data.title}
+              <a href={data.link} type="application/pdf" target="_blank">Link</a>
+            </div>
+          ))}
         </div>
       </BackgroundBeamsWithCollision>
     </div>
---
+++ src/utils/cards-data.js
@@ -0,0 +1,12 @@
+const CardsData = [
+    {
+        title: "DSA Sheet",
+        link: "https://neetcode.io/practice"
+    },
+    {
+        title: "Computer Network",
+        link: "./assets/Computer-Network.pdf"
+    }
+]
+
+export default CardsData;
